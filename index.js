c9b57e44fcf2292a4e5e1dd8226113ded4c996f3
amusing-constant-9311 <cmstie@gmail.com>
2019-10-10 22:56:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function dwarfRollCall(dwarves) {
+  var dwarf = ""
+  for (var i = 0; i < dwarves.length; i++) {
+   dwarf += (i+1).toString()+ ". " + dwarves[i] + " ";
+  }
+  return dwarf;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var planeteer = [];
+  for (var i = 0; i < planeteerCalls.length; i++) {
+    planeteer.push(planeteerCalls[i].toUpperCase() + "!");
+  }
+  return planeteer
 }
 
 function longPlaneteerCalls(words) {
+  var long = false
+  for (var i = 0; i < words.length; i++){
+    if (words[i].length > 4){
+      long = true
+    }
+  }
+  return long
 }
 
 function findTheCheese (foods) {
+  var cheese = ["cheddar", "gouda", "camembert"]
+  for (var i = 0; i < foods.length; i++){
+    for (var j = 0; j < cheese.length; j++){
+      if (foods[i]===cheese[j])
+      return foods[i]
+    }
+  }
+  return "no cheese!"
 }
